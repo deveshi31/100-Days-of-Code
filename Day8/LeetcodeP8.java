b9401f2c9294fa153212e9281ab98c906df11e6f
Deveshi Parashar <deveshi.parashar31@gmail.com>
2024-03-25 15:34:49 +0530

    Leetcode 442. Find All Duplicates in an Array

---
+++ Day8/LeetcodeP8.java
@@ -0,0 +1,21 @@
+package Day8;
+
+// Leetcode 442. Find All Duplicates in an Array
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+  public List<Integer> findDuplicates(int[] nums) {
+      List<Integer> ans = new ArrayList();
+      int n = nums.length;
+      for (int i = 0; i < n; i++) {
+          int x = Math.abs(nums[i]);
+          if (nums[x - 1] < 0) {
+              ans.add(x);
+          }
+          nums[x - 1] *= -1;
+      }
+      return ans;
+  }
+}
