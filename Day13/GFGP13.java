6a2d2f014e7e3bf164d63905f1186efb9bd5e2fb
Deveshi Parashar <deveshi.parashar31@gmail.com>
2024-03-30 02:41:14 +0530

    Minimum element in BST

---
+++ Day13/GFGP13.java
@@ -0,0 +1,136 @@
+package Day13;
+
+//Minimum element in BST
+
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Node {
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+
+class GfG {
+
+    static Node buildTree(String str) {
+
+        if (str.length() == 0 || str.charAt(0) == 'N') {
+            return null;
+        }
+
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+
+        Queue<Node> queue = new LinkedList<>();
+
+        queue.add(root);
+        // Starting from the second element
+
+        int i = 1;
+        while (queue.size() > 0 && i < ip.length) {
+
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+
+            // Get the current node's value from the string
+            String currVal = ip[i];
+
+            // If the left child is not null
+            if (!currVal.equals("N")) {
+
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+
+            // For the right child
+            i++;
+            if (i >= ip.length) break;
+
+            currVal = ip[i];
+
+            // If the right child is not null
+            if (!currVal.equals("N")) {
+
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+
+        return root;
+    }
+
+    static void printInorder(Node root) {
+        if (root == null) return;
+
+        printInorder(root.left);
+        System.out.print(root.data + " ");
+
+        printInorder(root.right);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine());
+
+        while (t > 0) {
+            String s = br.readLine();
+            Node root = buildTree(s);
+            Solution g = new Solution();
+            System.out.println(g.minValue(root));
+            t--;
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+
+
+class Solution {
+    // Function to find the minimum element in the given BST.
+    int minValue(Node root) {
+        // code here
+        int n1,n2;
+        
+        if(root.left==null && root.right==null)
+            return root.data;
+        else if(root.left==null){
+            n1=root.data;
+            n2=minValue(root.right);
+        }else if(root.right==null){
+            n1=minValue(root.left);
+            n2=root.data;
+        }else{
+            n1=minValue(root.left);
+            n2=minValue(root.right);
+        }
+        
+        
+        return Math.min(n1,n2);
+        
+    }
+}
+
