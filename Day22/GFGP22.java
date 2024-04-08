e7ad047167d57d5118080d65e0804b40598ba103
Deveshi Parashar <deveshi.parashar31@gmail.com>
2024-04-08 14:56:08 +0530

    Optimal Strategy For A Game

---
+++ Day22/GFGP22.java
@@ -0,0 +1,62 @@
+package Day22;
+
+// Optimal Strategy For A Game
+
+//{ Driver Code Starts
+  import java.util.*;
+  
+  class OptimalStrategy
+  {
+      public static void main (String[] args) {
+          
+          try (//taking input using Scanner class
+          Scanner sc = new Scanner(System.in)) {
+            //taking total number of testcases
+            int t = sc.nextInt();
+            
+            while(t-- > 0)
+            {
+                //taking number of elements
+                int n = sc.nextInt();
+                int arr[] = new int[n];
+                
+                //inserting the elements
+                for(int i = 0; i < n; i++)
+                    arr[i] = sc.nextInt();
+                    
+               new solve();
+              //calling the countMaximum() method of class solve
+               System.out.println(solve.countMaximum(n, arr)); 
+            }
+          }
+      }
+      
+      
+  }
+  // } Driver Code Ends
+  
+  
+  
+  
+  
+  class solve {
+      static long countMaximum(int n, int arr[]) {
+          int[][] dp = new int[n][n];
+          
+          for (int g = 0; g < n; g++) {
+              for (int i = 0, j = g; j < n; i++, j++) {
+                  if (g == 0) {
+                      dp[i][j] = arr[i];
+                  } else if (g == 1) {
+                      dp[i][j] = Math.max(arr[i], arr[j]);
+                  } else {
+                      dp[i][j] = Math.max(arr[i] + Math.min(dp[i + 2][j], dp[i + 1][j - 1]), 
+                                          arr[j] + Math.min(dp[i + 1][j - 1], dp[i][j - 2]));
+                  }
+              }
+          }
+          return dp[0][n - 1];
+      }
+  }
+
+
