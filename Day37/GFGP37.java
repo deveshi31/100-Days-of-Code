e6e400b2531ae49e4a2e569097a6049056664f1e
Deveshi Parashar <deveshi.parashar31@gmail.com>
2024-04-23 13:44:11 +0530

    GFG Rohan's Love for Matrix

---
+++ Day37/GFGP37.java
@@ -0,0 +1,43 @@
+package Day37;
+
+// GFG Rohan's Love for Matrix
+
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            int n = Integer.parseInt(read.readLine());
+            System.out.println(Solution.firstElement(n));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+
+
+// User function Template for Java
+class Solution {
+    static int firstElement(int n) {
+        // code here
+         int a = 1;
+        int b = 1;
+        int mod = 1000000007;
+        
+        if (n == 0 || n == 1) return 1;
+        
+        if (n == 2) return 2;
+        
+        for (int i = 2; i < n; i++) {
+            int temp = a;
+            a = (a + b) % mod;
+            b = temp;
+        }
+        return a;
+    }
+}
