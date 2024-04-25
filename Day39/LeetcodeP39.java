5015a04b2b4e50a75b9003320632a3824b6b6e8a
Deveshi Parashar <deveshi.parashar31@gmail.com>
2024-04-25 13:39:42 +0530

    Leetcode 2370. Longest Ideal Subsequence

---
+++ Day39/LeetcodeP39.java
@@ -0,0 +1,21 @@
+package Day39;
+
+// Leetcode 2370. Longest Ideal Subsequence
+
+class Solution {
+  public int longestIdealString(String s, int k) {
+      int[] dp = new int[26];
+      int ans=1;
+      for(int i=0;i<s.length();i++){
+          int ele = s.charAt(i) - 'a';
+          for(int j=ele;j>=0 && j>=ele-k;j--){
+              dp[ele] = Math.max(dp[ele],dp[j]+1);
+          }
+          for(int j=ele+1;j<26 && j<=ele+k;j++){
+              dp[ele] = Math.max(dp[ele],dp[j]+1);
+          }
+          ans = Math.max(ans,dp[ele]);
+      }
+      return ans;
+  }
+}
