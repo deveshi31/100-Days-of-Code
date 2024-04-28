7c0dfa75cd452ca458cf71edcfdd560b3e132ca0
Deveshi Parashar <deveshi.parashar31@gmail.com>
2024-04-28 22:02:19 +0530

    Leetcode 834. Sum of Distances in Tree

--- Day42/GFG42.java
+++ Day42/GFG42.java
@@ -27,21 +27,22 @@ package Day42;
       System.out.println(); 
     }
       public static void main(String args[]) throws IOException { 
-          Scanner sc = new Scanner(System.in);
-          int t = sc.nextInt();
-          while(t > 0){
-            int n = sc.nextInt();
-              Node head = new Node(sc.nextInt());
-              Node tail = head;
-              for(int i=0; i<n-1; i++)
-              {
-                  tail.next = new Node(sc.nextInt());
-                  tail = tail.next;
-              }
-              Solution g = new Solution();
-              head = g.deleteMid(head);
-              printList(head); 
-              t--;
+          try (Scanner sc = new Scanner(System.in)) {
+            int t = sc.nextInt();
+            while(t > 0){
+              int n = sc.nextInt();
+                Node head = new Node(sc.nextInt());
+                Node tail = head;
+                for(int i=0; i<n-1; i++)
+                {
+                    tail.next = new Node(sc.nextInt());
+                    tail = tail.next;
+                }
+                Solution g = new Solution();
+                head = g.deleteMid(head);
+                printList(head); 
+                t--;
+            }
           }
       } 
   } 
