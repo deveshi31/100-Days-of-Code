c390514a1e962ebbc3291d9e46d2afa0fd31e323
Deveshi Parashar <deveshi.parashar31@gmail.com>
2024-05-01 16:11:54 +0530

    Leetcode 2000. Reverse Prefix of Word

---
+++ Day45/LeetcodeP45.java
@@ -0,0 +1,13 @@
+package Day45;
+
+// Leetcode 2000. Reverse Prefix of Word
+
+class Solution {
+  public String reversePrefix(String word, char ch) {
+      int j = word.indexOf(ch);
+      if (j != -1) {
+          return new StringBuilder(word.substring(0, j + 1)).reverse().toString() + word.substring(j + 1);
+      }
+      return word;
+  }
+}
