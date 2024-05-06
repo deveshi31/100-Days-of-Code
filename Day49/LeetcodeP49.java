13a9407b732ed74057055fffceb5f6d30849f851
Deveshi Parashar <deveshi.parashar31@gmail.com>
2024-05-06 13:51:51 +0530

    GFG Print all nodes that don't have sibling

--- Day49/LeetcodeP49.java
+++ Day49/LeetcodeP49.java
@@ -2,14 +2,43 @@ package Day49;
 
 import java.util.List;
 
-// Leetcode 237. Delete Node in a Linked List
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
 
+ class ListNode {
+  int val;
+  ListNode next;
+  ListNode(int x) { val = x; }
+}
 class Solution {
   /**
    * @param node
    */
-  public void deleteNode(@SuppressWarnings("rawtypes") List node) {
+  public void deleteNode(ListNode node) {
+      // Edge case: If the node to be deleted is the last node, do nothing
+      if (node.next == null) {
+          return;
+      }
+
+      // Copy the value of the next node to the current node
       node.val = node.next.val;
+
+      
+
+      // Traverse to the next node
+      @SuppressWarnings("rawtypes")
+      List temp = (List) node.next;
+
+      // Update the next pointer of the current node
       node.next = node.next.next;
+
+      // Free the memory occupied by the deleted node
+      temp.next = null;
   }
 }
