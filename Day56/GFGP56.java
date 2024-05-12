7f37b2a591a51af9e2b1243625f6e567e2c84807
Deveshi Parashar <deveshi.parashar31@gmail.com>
2024-05-13 00:23:47 +0530

    GFG Minimum steps to destination

---
+++ Day56/GFGP56.java
@@ -0,0 +1,42 @@
+package Day56;
+
+// GFG Minimum steps to destination
+
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(in.readLine());
+        while (t-- > 0) {
+            int d = Integer.parseInt(in.readLine());
+
+            Solution ob = new Solution();
+            System.out.println(Solution.minSteps(d));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+
+
+
+
+// User function Template for Java
+
+class Solution {
+    static int minSteps(int d) {
+        // code here
+        int sum=0,diff=0,steps=0;
+        while(sum<d || diff%2!=0){
+            steps++;
+            sum+=steps;
+            diff=sum-d;
+        }
+        return steps;
+    }
+}
