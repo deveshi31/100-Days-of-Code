ee7991fae872d8e2bdeece73c6b7678e1132870d
Deveshi Parashar <95047118+deveshi31@users.noreply.github.com>
2024-06-03 12:36:53 +0530

    Leetcode 2486. Append Characters to String to Make Subsequence

---
+++ Day78/LeetcodeP78.java
@@ -0,0 +1,21 @@
+package Day78;
+
+// Leetcode 2486. Append Characters to String to Make Subsequence
+
+
+class Solution {
+  public int appendCharacters(String s, String t) {
+      int sIndex = 0, tIndex = 0;
+      int sLength = s.length(), tLength = t.length();
+      
+      while (sIndex < sLength && tIndex < tLength) {
+          if (s.charAt(sIndex) == t.charAt(tIndex)) {
+              tIndex++;
+          }
+          sIndex++;
+      }
+      
+      return tLength - tIndex;
+      
+  }
+}
