4e3dfa9e2ab90bcc76e9a3849b88b5ed448b4314
Deveshi Parashar <95047118+deveshi31@users.noreply.github.com>
2024-06-24 15:11:33 +0530

    Leetcode 995. Minimum Number of K Consecutive Bit Flips

---
+++ Day99/LeetcodeP99.java
@@ -0,0 +1,22 @@
+package Day99;
+
+// Leetcode 995. Minimum Number of K Consecutive Bit Flips
+
+class Solution {
+  public int minKBitFlips(int[] nums, int k) {
+      int n = nums.length, flipped = 0, res = 0;
+      int[] isFlipped = new int[n];
+      for (int i = 0; i < nums.length; ++i) {
+          if (i >= k)
+              flipped ^= isFlipped[i - k];
+              if (flipped == nums[i]) {
+                  if (i + k > nums.length)
+                      return -1;
+              isFlipped[i] = 1;
+              flipped ^= 1;
+              res++;
+          }
+      }
+      return res;
+  }
+}
